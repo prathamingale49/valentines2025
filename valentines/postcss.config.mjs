4dbdf03eeaf3a03041ffe40ee22626af5d8d82a7
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-12 23:07:55 -0500

    change

--- valentines/postcss.config.mjs
+++ valentines/postcss.config.mjs
@@ -1,9 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
-export default config;
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
