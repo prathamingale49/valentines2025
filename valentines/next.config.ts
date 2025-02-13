2984893a3e889231f66ada4e9f2c82d8bceacbd0
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-12 21:59:06 -0500

    githubs

--- valentines/next.config.ts
+++ valentines/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "/valentines2025/" : "",
+  basePath: isProd ? "/valentines2025" : "",
+  output: "export",
   /* config options here */
 };
 
